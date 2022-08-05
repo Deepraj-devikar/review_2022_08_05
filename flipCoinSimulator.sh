b7c00cf6dc305e363ca244d46f79e645b732aa99
Deepraj-devikar <51924773+Deepraj-devikar@users.noreply.github.com>
2022-08-05 21:02:42 +0530

    simulator uc2 arr code

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,6 +6,21 @@ simulator () {
 }
 
 won=$(simulator)
+wonArr=( $(simulator) $(simulator) $(simulator) $(simulator) $(simulator) )
+
+headWon=0
+tailWon=0
+for v in ${wonArr[@]}
+do
+	if [ $v -eq 0 ]
+	then
+		headWon=$(($headWon+1))
+	else
+		tailWon=$(($tailWon+1))
+	fi
+done
+echo "in array head won $headWon times and tail won $tailWon times";
+
 if [ $won -eq 0 ]
 then
 	echo "head won";
